d66033b8708e3a555eb526d51dddd52930b87604
Muhammadrasul <holturaevm@gmail.com>
2023-07-18 14:50:16 +0300

    finish the first task: creating a timer with given seconds, which is shown in hh.mm.ss format

---
+++ index.js
@@ -0,0 +1,46 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
+
+const createTimerAnimator = () => {
+  let intervalId;
+
+  return (seconds) => {
+    clearInterval(intervalId);
+
+    let remainingSeconds = seconds; 
+    const updateTimer = () => {
+      const hours = Math.floor(remainingSeconds / 3600);
+      const minutes = Math.floor((remainingSeconds % 3600) / 60);
+      const seconds = remainingSeconds % 60;
+
+      const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+      timerEl.textContent = formattedTime;
+
+      remainingSeconds--;
+
+      if (remainingSeconds < 0) {
+        clearInterval(intervalId);
+      }
+    };
+
+    updateTimer();
+
+    intervalId = setInterval(updateTimer, 1000);
+  };
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener('input', () => {
+  inputEl.value = inputEl.value.replace(/\D/g, '');
+});
+
+buttonEl.addEventListener('click', () => {
+  const seconds = Number(inputEl.value);
+
+  animateTimer(seconds);
+
+  inputEl.value = '';
+});
